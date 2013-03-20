ee778d58ea82729ce0f9017a9d0b28c6b1814144
Bo Li <prclibo@gmail.com>
2013-03-20 16:55:58 +0100

    removed non-used files

--- scripts/temp.m
+++
@@ -1,8 +0,0 @@
-path = '../image/5cameras/selected/'; 
-fileList = dir([path, '*.png']); 
-
-for i = 1:numel(fileList)
-    display(fileList(i).name); 
-    index = sscanf(fileList(i).name, '%d-%d.png'); 
-    system(['mv ', path, fileList(i).name, ' ', path, num2str(index(2)), '-', num2str(index(1)), '.png']); 
-end
